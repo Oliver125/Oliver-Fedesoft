54317176eecbe64cd58e94b46d27e8124d9af582
oliver <otaimbud@gmail.com>
2018-10-12 16:11:46 -0500

    carga2

--- Estructura/js/cuadromovible.js
+++ Estructura/js/cuadromovible.js
@@ -1,4 +1,14 @@
 
-var object= document.getElementById("caja");
-object.style="left: 150px"
-
+var object = document.getElementById("caja");
+var inicio = 0;
+for (var a = 0; a < 10000000; a++) {
+    var c = 200
+}
+//object.style="left: 100px"
+object.style.left = inicio + "px"
+for (var i = 0; i < 200; i++) {
+    object.style.left = inicio + "px"
+    inicio = inicio + 1;
+    
+}
+//alert(inicio)
